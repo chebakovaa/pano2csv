6feabb0650b389c2e09c28ca5f37827d770676fd
ChebakovAA <achbkv@gmail.com>
2021-05-14 17:42:19 +0500

    mocking TableCollectionTest

--- src/main/java/com/bisoft/models/FileTarget.java
+++ src/main/java/com/bisoft/models/FileTarget.java
@@ -5,17 +5,30 @@ import com.bisoft.interfaces.IFileFormat;
 import com.bisoft.interfaces.ISaveTarget;
 import com.bisoft.interfaces.ISavedFormat;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 public class FileTarget implements ISaveTarget {
+	private final IClearedFolder location;
+	private final IFileFormat format;
+	private OutputStreamWriter out;
+	
 	public FileTarget(IClearedFolder location, IFileFormat format) {
+		this.location = location;
+		this.format = format;
 	}
 	
 	@Override
 	public ISavedFormat savedFormat() {
+		String fn = location.
+		out = new OutputStreamWriter(new FileOutputStream(file.toString()+".csv"), "UTF8");
 		return null;
 	}
 	
 	@Override
 	public void close() throws Exception {
-	
+		if(out != null) {
+			out.close();
+		}
 	}
 }
